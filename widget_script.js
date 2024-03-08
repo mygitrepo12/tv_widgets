83e976499f2ac0e38f167de3008b0898b0bf60e2
mygitrepo12 <161922629+mygitrepo12@users.noreply.github.com>
2024-03-08 22:57:52 +0200

    Update widget_script.js

--- widget_script.js
+++ widget_script.js
@@ -1,27 +1,6 @@
-    new TradingView.widget(
-        {
-            "container_id": "tv-chart",
-            "width": "100%",
-            "height": "100%",
-            "autosize": true,
-            "symbol": "CHKP",
-            "interval": "D",
-            "timezone": "exchange",
-            "theme": "dark",
-            "style": "1",
-            "withdateranges": true,
-            "hide_side_toolbar": false,
-            "allow_symbol_change": true,
-            "save_image": false,
-            "studies": [
-                "ROC@tv-basicstudies",
-                "StochasticRSI@tv-basicstudies",
-                "MASimple@tv-basicstudies"
-            ],
-            "show_popup_button": true,
-            "popup_width": "1000",
-            "popup_height": "650",
-            "support_host": "https://www.tradingview.com"
-
-        }
-    )
+      var a = document.createElement('a');
+      var linkText = document.createTextNode("my title text");
+      a.appendChild(linkText);
+      a.title = "my title text";
+      a.href = "http://example.com";
+      document.body.appendChild(a);
